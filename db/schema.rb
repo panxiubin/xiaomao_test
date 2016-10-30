833ccb399a92a2676dd17a81c91efe15029eb410
panxiubin <xiubinpan@gmail.com>
2016-10-30 10:59:46 +0800

    add model student

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030023724) do
+ActiveRecord::Schema.define(version: 20161030025647) do
 
   create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
