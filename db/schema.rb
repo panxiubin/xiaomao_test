4626b2f8ac674aaa4ee60c8ee5204b9b5801fbaa
panxiubin <xiubinpan@gmail.com>
2016-10-30 11:06:49 +0800

    add teacher_id to courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030030319) do
+ActiveRecord::Schema.define(version: 20161030030550) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "teacher_id"
   end
 
   create_table "sc_relations", force: :cascade do |t|
