f87d161f2062066cf9a546341b1484399ce460bf
panxiubin <xiubinpan@gmail.com>
2016-10-30 11:04:48 +0800

    add model st_relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030025956) do
+ActiveRecord::Schema.define(version: 20161030030319) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sc_relations", force: :cascade do |t|
+    t.integer  "student_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "st_relations", force: :cascade do |t|
+    t.integer  "student_id"
+    t.integer  "teacher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
